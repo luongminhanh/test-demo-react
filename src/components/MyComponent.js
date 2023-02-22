5db179d3ffcf55b53b4bd631f9dc3377d56c39fe
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-02-22 22:35:09 +0700

    b16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MyComponent extends React.Component {
+    render(){
+        return (
+            <div>My first Component
+                
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
